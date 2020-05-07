60a2e57137eb280d0b7bea371b1f7e4b1961adc1
Jean-Christophe Robertson <35150425+Jean-ChristopheRobertson@users.noreply.github.com>
2020-05-06 21:18:31 -0700

    setting up jest, enzyme etc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jean-Christophe Robertson's personal website!
-        </p>
+        <p className="name">Jean-Christophe Robertson's personal website!</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
