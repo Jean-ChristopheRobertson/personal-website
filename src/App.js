8c2869f083a073df79d99071a58df6ca5b4825cc
Jean-Christophe Robertson <35150425+Jean-ChristopheRobertson@users.noreply.github.com>
2020-05-06 20:05:28 -0700

    create react app output

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jean-Christophe Robertson's personal website!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
