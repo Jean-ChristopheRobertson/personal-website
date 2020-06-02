f546109aba3879d7cc4c35b373c35c2f8031a9b2
Jean-Christophe Robertson <35150425+Jean-ChristopheRobertson@users.noreply.github.com>
2020-06-01 20:53:41 -0700

    Adding bootstrap header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from "react"
+import { Navbar, NavDropdown, Nav } from "react-bootstrap"
 
 const Header = () => {
   return (
     <>
-      <h1>Header</h1>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Jean-Christophe Robertson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Do</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">All </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">The Things!</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                One mooooore thing
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">LinkedIn</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   )
 }
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,7 @@
+import Header from "./Header"
+
+describe("Header testing suite", () => {
+  it("Should be defined", () => {
+    expect(Header).toBeDefined()
+  })
+})
