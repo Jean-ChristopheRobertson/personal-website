325fbfee932abedfb043eb0475dc10c860d07040
Jean-Christophe Robertson <35150425+Jean-ChristopheRobertson@users.noreply.github.com>
2021-02-02 20:50:16 -0800

    cleaning up some CRA stuff

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,31 @@
-import React from "react"
-import { Navbar, NavDropdown, Nav } from "react-bootstrap"
+import React from "react";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Jean-Christophe Robertson</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Do</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">All </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">The Things!</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                One mooooore thing
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">LinkedIn</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
-  )
-}
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Jean-Christophe Robertson</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Do</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">All </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">The Things!</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              One mooooore thing
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">LinkedIn</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,7 +1,15 @@
-import Header from "./Header"
+import Header from "./Header";
+import { shallow } from "enzyme";
+import React from "react";
+import { Navbar } from "react-bootstrap";
 
 describe("Header testing suite", () => {
   it("Should be defined", () => {
-    expect(Header).toBeDefined()
-  })
-})
+    expect(Header).toBeDefined();
+  });
+
+  const wrapper = shallow(<Header />);
+  it("Should contain the navbar", () => {
+    expect(wrapper.find(Navbar)).toHaveLength(1);
+  });
+});
